8e6f35cf30f7afcb61c87c74e365aa45f7b41c33
Is0lde <simon.tanguay@hotmail.ca>
2026-05-26 15:50:05 -0400

    Updated diceroller

--- cogs/roller.py
+++ cogs/roller.py
@@ -3,18 +3,18 @@ from discord.ext import commands
 from discord import app_commands
 import random
 import diceShape
+from diceShape import DiceShape
 
 class Diceroll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-       #@app_commands.describe(nom_du_param="la description a montrer le user")
-        @app_commands.command(name="diceroll",description="Roll a dice.")
-        @app_commands.describe(shape="Dice shape to roll")
-        async def diceroll(interaction: discord.Interaction, shape: diceShape.DiceShape):
-            randInt = random.randint(1,shape.value)
-
-            embed = discord.Embed(title="Dice roll results", description=f"You rolled a {randInt}!", color=discord.Color.purple())
-            interaction.response.send_message(embed=embed)
+   #@app_commands.describe(nom_du_param="la description a montrer le user")
+    @app_commands.command(name="diceroll",description="Roll a dice.")
+    @app_commands.describe(shape="Dice shape to roll")
+    async def diceroll(self, interaction: discord.Interaction, shape: diceShape.DiceShape):
+        randInt = random.randint(1,shape.value)
+        embed = discord.Embed(title="Dice roll results", description=f"You rolled a {randInt}!", color=discord.Color.purple())
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot):
