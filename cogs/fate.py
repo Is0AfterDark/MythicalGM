8eb0223f8bf78c1028969ce30ce12cd28191c6dd
Is0lde <simon.tanguay@hotmail.ca>
2026-05-28 13:41:34 -0400

    Fixed the yes/no system to comply with game rules again...again

--- cogs/fate.py
+++ cogs/fate.py
@@ -100,10 +100,12 @@ class Fatecheck(commands.Cog):
             await randomEvent(randInt)
         elif randInt <= FATECRITS[chance][chaos - 1]:
             result = "Exceptional yes!"
-        elif randInt > FATESUCCESS[chance][chaos - 1] or randInt ==0:
+        elif randInt > FATESUCCESS[chance][chaos - 1]:
             result = "No."
         elif randInt >= FATEFAILS[chance][chaos - 1]:
             result = "Exceptional no!"
+        elif FATESUCCESS[chance][chaos - 1] == 0:
+            result = "Yes."
         else:
             result = "Yes."
 
